7439a3fa76f769620339703ec64111168ea2a4cf
Pietro Di Bello <pierodibello@gmail.com>
2021-04-06 16:27:47 +0200

    refactor unit test

--- src/test/java/domain/PostMessageUseCaseTest.java
+++ src/test/java/domain/PostMessageUseCaseTest.java
@@ -1,18 +1,20 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class PostMessageUseCaseTest {
 
+    private final MessageRepository messageRepository = mock(MessageRepository.class);
+
     @Test
     void a_user_can_post_a_message() {
-        MessageRepository messageRepository = Mockito.mock(MessageRepository.class);
         PostMessageUseCase postMessage = new PostMessageUseCase(messageRepository);
         Username username = new Username("Alice");
         Message message = new Message("I love the weather");
+
         postMessage.postMessage(username, message);
 
         verify(messageRepository).addMessageForUser(username, message);
