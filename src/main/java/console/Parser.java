cf989156c36e99cf4b8830e4bc267deb3dbc1aae
Pietro Di Bello <pierodibello@gmail.com>
2021-05-04 22:55:43 +0200

    refactor the parser

--- src/main/java/console/Parser.java
+++ src/main/java/console/Parser.java
@@ -1,10 +1,19 @@
 package console;
 
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
 public class Parser {
     public Command parse(String string) {
-        String[] tokens = string.split(" -> ");
-        String username = tokens[0];
-        String message = tokens.length == 2 ? tokens[1] : null;
+        List<String> tokens = stream(string.split("->"))
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .collect(toList());
+        String username = tokens.get(0);
+        String message = tokens.size() == 2 ? tokens.get(1) : null;
         return new Command(username, message);
     }
 }
